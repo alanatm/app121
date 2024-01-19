10ae0ec1883f43a59f7270e68bb12635ecd647ad
Alan <roalan96@gmail.com>
2024-01-19 12:49:35 -0600

    restauracion

--- node/database/db.js
+++ node/database/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize';
 
-const db = new Sequelize('railway', 'railway','TGYgAY4GJNFJ4fv-UkcbDh_bVG5AtCEQ',{
-    host: 'viaduct.proxy.rlwy.net',
+const db = new Sequelize('contactcenter121', 'root','',{
+    host: 'localhost',
     dialect: 'mysql'
 });
 
